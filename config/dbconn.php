3bc9def820f9133f98a00ad4e8dbe3a332ef96ad
dlcincluded <carydavid85@gmail.com>
2021-09-21 19:17:23 -0400

    Basic DB connection
    Config and DB connection setup

---
+++ config/dbconn.php
@@ -0,0 +1,7 @@
+<?PHP
+	require("config.php");
+	$conn = new mysqli('localhost', $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE, NULL);
+	if($conn->connect_error){
+		die('Connection Error ('. $conn->connect_errno . ') ' . $conn->connect_error);
+	}//if we get no error, we have a successfull connection
+?>
